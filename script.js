e8d856ed23112c0a136dc604174d8eae8fea98f5
stefanyalvesomeli <stefanyalv.alves@mercadolivre.com>
2026-09-18 08:05:13 -0300

    Controle de equiapamentos

--- script.js
+++ script.js
@@ -3763,6 +3763,8 @@ window.limparFiltrosQuebrados =
   limparFiltrosQuebrados;
 
 
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@1"></script>
+
 /* =========================================================
    FIM DO SCRIPT
    ========================================================= */
